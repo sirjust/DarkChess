1c75980e5332fbc1f6870415ca9aa17e0276d7a7
Damian Dorosz <doroszdam@gmail.com>
2020-12-04 20:25:41 +0100

    Add Settings to the Title Screen and make game automatically save player settings

---
+++ Assets/Dev/Exostin/ExostinScripts/PlayerPrefsInitialize.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerPrefsInitialize : MonoBehaviour
+{
+    private int usedFullScreenMode;
+
+    private void Awake()
+    {
+        if (!PlayerPrefs.HasKey("FullScreenMode"))
+        {
+            PlayerPrefs.SetInt("FullScreenMode", 1);
+            PlayerPrefs.Save();
+        }
+        usedFullScreenMode = PlayerPrefs.GetInt("FullScreenMode");
+        SetFullScreenMode(usedFullScreenMode);
+    }
+
+    public void SetFullScreenMode(int fullScreenModeIndex)
+    {
+        switch (fullScreenModeIndex)
+        {
+            case 0:
+                Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+                Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
+                break;
+
+            case 1:
+                Screen.fullScreenMode = FullScreenMode.Windowed;
+                Screen.SetResolution(1280, 720, false);
+                break;
+        }
+    }
+}
