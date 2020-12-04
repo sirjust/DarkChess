b12282f0a49d6e73d6f01648b7bf954a0a177e58
Damian Dorosz <18118467+exostin@users.noreply.github.com>
2020-12-04 22:55:17 +0000

    "Create title page", "Make settings page in Title Screen" tickets (#2)
    * Add documentation of GetVersion in TitleScreen scene and removed link to Exostin Twitter
    
    * Import TM Pro Essentials, make basic TitleScreen
    
    * Unity deleted those dunno
    
    * Version text will not disappear once another scene is loaded
    
    * Add Settings to the Title Screen and make game automatically save player settings
    
    * Add TitleScreen as the first, and GathdarDev as the second scene in build settings
    
    * Make the game lauch itself in Windowed mode, as we do not even have pause menu yet
    
    * Add files that Unity automatically generated after cooking the first Build

---
+++ Assets/Dev/Exostin/ExostinScripts/GetVersion.cs
@@ -0,0 +1,12 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class GetVersion : MonoBehaviour
+{
+    public TextMeshProUGUI versionText;
+
+    private void Start()
+    {
+        versionText.text = "Build v" + Application.version;
+    }
+}
---
+++ Assets/Dev/Exostin/ExostinScripts/TitleScreenHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenHandler : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        // You need to put the number of the main scene from build settings in here
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
