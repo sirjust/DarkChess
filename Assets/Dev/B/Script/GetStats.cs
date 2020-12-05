dd376868a373fad95edf2fd2da8a6ddc73622869
unbekanntunity <68324862+unbekanntunity@users.noreply.github.com>
2020-12-05 20:30:42 +0100

    Add BattleMenu (#3)
    * Add BattleMenu
    
    * Modify BattleMenu
    
    * Changed variables
    
    * modify Battle menu
    
    * Modify Battle Menu
    
    * Modify Battle Menu
    
    * Modifiy Battle Menu

---
+++ Assets/Dev/B/Script/GetStats.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public enum HealthRepresentation
+{
+    healthbar, hearts
+}
+
+[RequireComponent(typeof(BoxCollider))]
+public class GetStats : MonoBehaviour
+{
+    public Character character;
+    public CharInfo charInfo;
+
+    public HealthRepresentation healthRepresentation;
+    public Sprite heartsPic;
+    public int size = 4;
+
+    private BoxCollider boxCollider;
+    private GameObject body;
+    private GameObject[] allObj;
+    private Slider healthbar;
+    private Image heartsContainer;
+
+    private void Awake()
+    {
+        healthbar = GetComponentInChildren<Slider>();
+        heartsContainer = GetComponentInChildren<Image>();
+
+        allObj = FindObjectsOfType<GameObject>();
+        foreach (GameObject _gameObject in allObj)
+        {
+            if (_gameObject.GetComponent<CharInfo>())
+            {
+                charInfo = _gameObject.GetComponent<CharInfo>();
+            }
+        }
+
+        boxCollider = GetComponent<BoxCollider>();
+        boxCollider.size = new Vector3(size, size, size);
+
+        var charObj = Instantiate(character.Model, this.gameObject.transform);
+        charObj.transform.SetParent(this.gameObject.transform);
+        body = charObj;
+
+        if(healthRepresentation == HealthRepresentation.healthbar)
+        {
+            heartsContainer.enabled = false;
+        }
+        else
+        {
+            heartsContainer.enabled = false;
+        }
+
+        charInfo.DisableMenu(false);
+    }
+
+    private void Update()
+    {
+        this.gameObject.transform.position = body.transform.position;
+    }
+
+    private void OnMouseDown()
+    {
+        charInfo.getCharID(character);
+        charInfo.RefreshStats(character);
+    }
+}
