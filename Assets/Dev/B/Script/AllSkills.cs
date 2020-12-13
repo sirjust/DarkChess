5254f3c83c59fa3dd99df73a2650191df230b45b
unbekanntunity <68324862+unbekanntunity@users.noreply.github.com>
2020-12-13 17:51:24 +0100

    Add visual healthbar/hearts (#9)
    * Add BattleMenu
    
    * Modify BattleMenu
    
    * Changed variables
    
    * modify Battle menu
    
    * Modify Battle Menu
    
    * Modify Battle Menu
    
    * Modifiy Battle Menu
    
    * Add visual Healthbar
    
    * change names/move folders
    
    * Fixed resolution problem
    
    * Fix resolution problem
    
    * Add SelectCard Functionality
    
    * Modify Highlight Mechanic
    
    * Modify Highlight Mechanic
    
    * Add Function to display SkillInfos
    
    * Modify Battle Menu, Add healthbar
    
    * Fixed NullError in the target system

---
+++ Assets/Dev/B/Script/AllSkills.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum Skills
+{
+    strike, move
+}
+
+public class AllSkills : MonoBehaviour
+{
+    private GameObject user;
+    private EditedGridGenerator gridGenerator;
+    private int targets = 0;
+
+    public bool cast(Card card, EditedGridGenerator _gridGenerator, GameObject _user)
+    {
+        user = _user;
+        gridGenerator = _gridGenerator;
+
+        foreach(GameObject tile in gridGenerator.selectedTiles)
+        {
+            foreach(GameObject tile1 in gridGenerator.skillrangeTiles)
+            {
+                Debug.Log($"{tile.transform.position.x} == { tile1.transform.position.x} && { tile.transform.position.z} == { tile1.transform.position.z}");
+                if (tile.transform.position.x == tile1.transform.position.x && tile.transform.position.z == tile1.transform.position.z)
+                {
+                    this.SendMessage(card.skill.ToString(), tile1);
+                    targets++;
+
+                    if (targets >= card.maxAmountOfTargets) return true;
+                }
+            }
+        }
+        if (targets == 0)
+        {
+            Debug.LogError("Select other tiles");
+            return false;
+        }
+
+        return true;
+    }
+
+    public void strike(GameObject targetTile)
+    {
+        Debug.Log($"strike at {targetTile.GetComponent<GetObjectonTile>().gameObjectOnTile.name}");
+    }
+
+    public void move()
+    {
+        Debug.Log("move");
+    }
+}
