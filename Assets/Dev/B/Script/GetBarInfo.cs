9f048efcbf884f30e6759315b4f76f6616034582
unbekanntunity <nguyen3lihn@gmail.com>
2020-12-13 21:32:14 +0100

    delete identity script

---
+++ Assets/Dev/B/Script/GetBarInfo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GetBarInfo : MonoBehaviour
+{
+    [Header("Requiered")]
+    public Character player;
+
+    public Slider healthbar;
+    public Slider manahbar;
+
+    private void Awake()
+    {
+        RefreshBar();
+    }
+    public void RefreshBar()
+    {
+        healthbar.maxValue = player.health;
+        healthbar.value = player.currentHealth;
+        manahbar.maxValue = player.mana;
+        manahbar.value = player.currentMana;
+    }
+}
