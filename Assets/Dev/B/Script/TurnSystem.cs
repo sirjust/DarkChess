71192c871541cb7ab5d6eb21cfe8a5d346a9da82
unbekanntunity <nguyen3lihn@gmail.com>
2020-12-19 12:26:55 +0100

    Implement Turn System

---
+++ Assets/Dev/B/Script/TurnSystem.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public enum BattleStatus
+{
+    PlayerMove, PlayerCombat, EnemyMove, EnemyCombat
+}
+
+public class TurnSystem : MonoBehaviour
+{
+    [Header("Requiered")]
+    public BattleStatus status;
+
+    private int index = 0;
+
+    private void Awake()
+    {
+        index = (int)status;
+    }
+
+    public void NextTurn()
+    {
+        if (index < 3) index++;
+        else index = 0;
+
+        status = (BattleStatus)index;
+    }
+
+    public void BackTurn()
+    {
+
+        if (index > 0) index--;
+        else index = 3;
+
+        status = (BattleStatus)index;
+
+    }
+
+    public void GetBattleStatus()
+    {
+        Debug.Log($"BattleStatus: {status}");
+    }
+}
