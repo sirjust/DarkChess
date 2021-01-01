a213cf4374c8911cafb671269e0a9bd8d5883d5f
unbekanntunity <68324862+unbekanntunity@users.noreply.github.com>
2021-01-01 14:39:28 +0100

    34 enemy ai (#37)
    * Add basic enemyAI, adapted gridgenerator.cs and allskills.cs
    
    * add enemy move mechanism
    
    * shortened code for enemy move ai
    
    * Implement basic AI
    
    * Modified basic AI
    
    * Modified basic AI
    
    * Modified basic AI
    
    * created and deleted GetObjectonTile script
    
    * changed allias to ally in TypeofTarget
    
    * Update branch with main (#34)
    
    * 34 enemy ai (#32)
    
    * Add basic enemyAI, adapted gridgenerator.cs and allskills.cs
    
    * add enemy move mechanism
    
    * shortened code for enemy move ai
    
    * Implement basic AI
    
    * Modified basic AI
    
    * Modified basic AI
    
    * Modified basic AI
    
    * created and deleted GetObjectonTile script
    
    * changed allias to ally in TypeofTarget
    
    * 60 fixed issue around gridgenerator (#33)
    
    * Add basic enemyAI, adapted gridgenerator.cs and allskills.cs
    
    * add enemy move mechanism
    
    * shortened code for enemy move ai
    
    * Implement basic AI
    
    * Modified basic AI
    
    * Modified basic AI
    
    * Modified basic AI
    
    * created and deleted GetObjectonTile script
    
    * changed allias to ally in TypeofTarget
    
    * update branch with main (#36)
    
    * 34 enemy ai (#32)
    
    * Add basic enemyAI, adapted gridgenerator.cs and allskills.cs
    
    * add enemy move mechanism
    
    * shortened code for enemy move ai
    
    * Implement basic AI
    
    * Modified basic AI
    
    * Modified basic AI
    
    * Modified basic AI
    
    * created and deleted GetObjectonTile script
    
    * changed allias to ally in TypeofTarget
    
    * 60 fixed issue around gridgenerator (#33)
    
    * Add basic enemyAI, adapted gridgenerator.cs and allskills.cs
    
    * add enemy move mechanism
    
    * shortened code for enemy move ai
    
    * Implement basic AI
    
    * Modified basic AI
    
    * Modified basic AI
    
    * Modified basic AI
    
    * created and deleted GetObjectonTile script
    
    * changed allias to ally in TypeofTarget
    
    * 60 fixed issue around gridgenerator (#35)
    
    * Add basic enemyAI, adapted gridgenerator.cs and allskills.cs
    
    * add enemy move mechanism
    
    * shortened code for enemy move ai
    
    * Implement basic AI
    
    * Modified basic AI
    
    * Modified basic AI
    
    * Modified basic AI
    
    * created and deleted GetObjectonTile script
    
    * changed allias to ally in TypeofTarget
    
    * added intial Values for charInfo
    
    * Add new behavior in enenmy movement
    
    * Add new behavior in case of no targets in range
    
    * Add new behavior in case of no targets in range
    
    * Removed debugs
    
    * Modified enemies behaviour
    
    * Modified enemies behaviour

--- Assets/Dev/B/Script/EnemyAI.cs
+++ Assets/Dev/B/Script/EnemyAI.cs
@@ -5,6 +5,12 @@ public class EnemyAI : MonoBehaviour
 {
     public List<GameObject> tempList = new List<GameObject>();
     public List<GameObject> tempList2 = new List<GameObject>();
+    public List<Vector3> playerPos = new List<Vector3>();
+    public int limitHealthPercent;
+    public GameObject closestTile;
+    public GameObject furthermostTile;
+    public float smallestDistance;
+    public float largestDistance;
 
     private TurnSystem turnSystem;
     private GetStats getStats;
@@ -29,7 +35,6 @@ public class EnemyAI : MonoBehaviour
             usedCard = PickRndCard(getStats.normalskills);
             ClearLists();
             EnemyMove();
-            tracked = false;
         }
         else if (turnSystem.GetBattleStatus() == BattleStatus.EnemyCombat && !tracked)
         {
@@ -57,8 +62,6 @@ public class EnemyAI : MonoBehaviour
         }
         gridGenerator.DestroyTiles(DestroyOption.rangeTiles, true, false);
 
-        rotation = (360 - (360 - (int)this.transform.localEulerAngles.y)) / 90;
-
         foreach (GameObject temptile in tempList)
         {
             for (int m = 0; m < 4; m++)
@@ -74,32 +77,62 @@ public class EnemyAI : MonoBehaviour
                         }
                 }
                 gridGenerator.DestroyTiles(DestroyOption.rangeTiles, true, false);
-
                 CheckRotation(placeHolder);
-
             }
             placeHolder.transform.localEulerAngles = new Vector3(0, 270, 0);
         }
-        tempList2.AddRange(gridGenerator.selectedTiles.ToArray());
 
-        if (tempList2.Count == 0)
+        playerPos = FindEnemyPos();
+
+        int indexPlayerPos = Random.Range(0, playerPos.Count - 1);
+
+        closestTile = tempList[0];
+        furthermostTile = tempList[0];
+        smallestDistance = Vector3.Distance(closestTile.transform.position, playerPos[indexPlayerPos]);
+        largestDistance = Vector3.Distance(furthermostTile.transform.position, playerPos[indexPlayerPos]);
+
+        foreach (GameObject rangeTile in tempList)
         {
-            //Placeholder
-            tempList2.Add(tempList[Random.Range(0, tempList.Count - 1)]);
+            if (Vector3.Distance(rangeTile.transform.position, playerPos[indexPlayerPos]) < smallestDistance)
+            {
+                smallestDistance = Vector3.Distance(rangeTile.transform.position, playerPos[indexPlayerPos]);
+                closestTile = rangeTile;
+            }
+            if (Vector3.Distance(rangeTile.transform.position, playerPos[indexPlayerPos]) > largestDistance)
+            {
+                largestDistance = Vector3.Distance(rangeTile.transform.position, playerPos[indexPlayerPos]);
+                furthermostTile = rangeTile;
+            }
+        }
+
+        if (limitHealthPercent != 0)
+        {
+            if (getStats.character.currentHealth < (getStats.character.health / 100) * limitHealthPercent)
+            {
+                tempList2.Add(furthermostTile);
+            }
+            else
+            {
+                if (gridGenerator.selectedTiles.Count == 0)
+                    tempList2.Add(closestTile);
+
+                tempList2.AddRange(gridGenerator.selectedTiles.ToArray());
+
+            }
         }
 
         RemoveDuplictas(tempList2);
         gridGenerator.DestroyTiles(DestroyOption.all, true, false);
+
         for (int i = 0; i < 4; i++)
         {
-            int index = Random.Range(0, tempList2.Count - 1);
-            rotation = (360 - (360 - (int)this.transform.localEulerAngles.y)) / 90;
+            int indexTempList2 = Random.Range(0, tempList2.Count - 1);
 
-            gridGenerator.selectedTiles.Add(tempList2[index]);
+            gridGenerator.selectedTiles.Add(tempList2[indexTempList2]);
             gridGenerator.GenerateSkillTiles(getStats.character.movementCard.ranges, getStats.character.movementCard.targetType, gameObject, TypesofValue.relative, false);
             if (allSkills.cast(getStats.character.movementCard, gridGenerator, gameObject, BattleStatus.EnemyMove))
             {
-                tempList2.RemoveAt(index);
+                tracked = false;
                 break;
             }
             CheckRotation(gameObject);
@@ -114,9 +147,9 @@ public class EnemyAI : MonoBehaviour
         {
             gridGenerator.GenerateSkillTiles(usedCard.ranges, usedCard.targetType, gameObject, TypesofValue.relative, false);
             tempList.AddRange(gridGenerator.rangeTiles.ToArray());
-            foreach(GameObject rangeTile in tempList)
+            foreach (GameObject rangeTile in tempList)
             {
-                if(rangeTile.GetComponent<GetObjectonTile>().gameObjectOnTile != null)
+                if (rangeTile.GetComponent<GetObjectonTile>().gameObjectOnTile != null)
                 {
                     if (rangeTile.GetComponent<GetObjectonTile>().gameObjectOnTile.GetComponent<GetStats>())
                     {
@@ -124,13 +157,38 @@ public class EnemyAI : MonoBehaviour
                     }
                 }
             }
-            if(allSkills.cast(usedCard, gridGenerator, gameObject, BattleStatus.EnemyCombat))
+            if (allSkills.cast(usedCard, gridGenerator, gameObject, BattleStatus.EnemyCombat))
             {
                 tracked = false;
                 break;
             }
             CheckRotation(gameObject);
         }
+        if (gridGenerator.selectedTiles.Count == 0)
+        {
+            tracked = false;
+            turnSystem.NextTurn();
+        }
+    }
+
+    public List<Vector3> FindEnemyPos()
+    {
+        List<Vector3> positions = new List<Vector3>();
+        List<GetObjectonTile> getObjectonTiles = new List<GetObjectonTile>();
+
+        getObjectonTiles.AddRange(FindObjectsOfType<GetObjectonTile>());
+
+        foreach (GetObjectonTile getObjectonTile in getObjectonTiles)
+        {
+            if (getObjectonTile.gameObjectOnTile != null)
+            {
+                if (getObjectonTile.gameObjectOnTile.GetComponent<GetStats>().character.realtion != RealtionType.Friendly)
+                {
+                    positions.Add(getObjectonTile.gameObjectOnTile.transform.localPosition);
+                }
+            }
+        }
+        return positions;
     }
 
     public Card PickRndCard(Card[] cards)
@@ -154,7 +212,7 @@ public class EnemyAI : MonoBehaviour
             {
                 if (list[m].transform.position == list[n].transform.position)
                 {
-                    list.RemoveAt(n);
+                    list.Remove(list[n]);
                 }
             }
         }
@@ -164,5 +222,7 @@ public class EnemyAI : MonoBehaviour
     {
         tempList.Clear();
         tempList2.Clear();
+        playerPos.Clear();
     }
 }
+
